21aa303115d8d70640bac29ab8a7e718b82e40ae
Nodira Ibragimova <nodira.ibrogimova@mail.ru>
2021-10-02 23:59:17 +0500

    Time: 24 ms (96.68%), Space: 14.3 MB (55.05%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,26 +1,36 @@
 from collections import deque
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        
+        # Approach 2: Optimization with answer in discuss part
+        pascal_tri = []
+        row, col = 0, 0
+        while row < numRows:
+            result = []
+            col = 0
+            while col < row + 1:
+                if col == 0 or col == row:
+                    result.append(1)
+                else:
+                    left = pascal_tri[row-1][col-1]
+                    right = pascal_tri[row-1][col]
+                    result.append(left+right)
+                col += 1
+            pascal_tri.append(result)
+            row += 1
+        return pascal_tri
+
+        # Approach 1: Brute-force solution
         '''
-        1. numRows => grid.length == numRows
-           curr_row = grid[row]
-           
              1
             1 1
            1 2 1        
-          1 3  3 1
+          1 3 3 1
          1 4 6 4 1
         1 5 10 10 5 1
        1 6 15 20 15 6 1
       1 7 21 35 35 21 7 1
-           
-        
-        1. Brute-force
-        2. Optimize
         '''
         grid = [[1]]
-        
         if numRows == 1:
             return grid
         row = 1
@@ -32,17 +42,8 @@ class Solution:
                 result.append(grid[row-1][left] + grid[row-1][right])
                 left += 1
                 right += 1
-            result.append(1)
-            result.appendleft(1)
-            grid.append(result)
+            grid.append(deque([1]) + result + deque([1]))
             row += 1
         return grid
-        
-        
-        
-        
-        
-    
-    
-        
-        
+    # Time complexity: O(n*m)
+    # Space complexity: O(30) - O(1)
