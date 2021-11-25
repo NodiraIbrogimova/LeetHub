eb4a1cb5c3f0f6909a8e0e581723a7d3a414089d
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2021-11-25 23:36:32 +0500

    Time: 32 ms (58.82%), Space: 14.3 MB (35.60%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.py
@@ -1,5 +1,15 @@
 class Solution:
     def numberOfMatches(self, n: int) -> int:
+        # Approach 2
+        # For the example of n = 126, total of 125 matches are played
+        # That's total of n-1 matches are played until one team is left
+        # More examples can be checked with different [n]s
+        return n-1
+        
+        # Approach 1
+        # Check for odd and even type of integer, update the match and advances
+        # Time: O(n)
+        # Space: O(1)
         matches, advances = 0, 0
         while n > 1:
             match = n//2
