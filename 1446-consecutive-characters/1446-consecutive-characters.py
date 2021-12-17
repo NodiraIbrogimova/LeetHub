af1bb003b1469d124ab3427ab5b8a1a6a3ca7f24
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2021-12-17 15:20:07 +0500

    Time: 76 ms (5.12%), Space: 14.3 MB (44.18%) - LeetHub

--- 1446-consecutive-characters/1446-consecutive-characters.py
+++ 1446-consecutive-characters/1446-consecutive-characters.py
@@ -1,6 +1,19 @@
 class Solution:
     def maxPower(self, s: str) -> int:
         
+        # Approach 2
+        # Count the consecutive char and once different chars are met, update maxCount if bigger than the curr_count
+        curr_count, max_count = 0, 0
+        for i in range(1, len(s)):
+            if s[i-1] == s[i]:
+                curr_count += 1
+            else:
+                max_count = max(curr_count, max_count)
+                curr_count = 0
+        return max(max_count+1, curr_count+1)
+                
+                
+        
         # Approach 1
         # As the domain of the chars are only lowercase English letters, we use a counter array with a length of 26 to count the frequency of the repeated substring chars
         # Return the max of the counter array as the power
@@ -8,7 +21,6 @@ class Solution:
         # Space: O(26) -> O(1) for an array of 26
         if len(s) == 1:
             return 1
-        
         count = [0]*26
         a = ord('a')
         prev, curr = 0, 1
