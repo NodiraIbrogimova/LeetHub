95864cac874d272d770eae3554e0a0d1d32c6e8f
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2021-12-20 04:46:10 +0500

    Time: 80 ms (12.64%), Space: 14.3 MB (98.94%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -0,0 +1,30 @@
+class Solution:
+    def isAlphaNumeric(self, value):
+        return (ord('A') <= ord(value) <= ord('Z')) or (ord('a') <= ord(value) <= ord('z')) or (ord('0') <= ord(value) <= ord('9'))
+    
+    def isPalindrome(self, s: str) -> bool:
+        '''
+        What are the characteristics of a palindrome?
+        1. Check if a value is alphanumeric for both left and right pointers.
+            Iterate through until both left and right are alphanumeric values
+        2. Convert found alphanumeric values to lower to check if they are equal
+            Directly return False once not equal has been found
+        '''
+        left, right = 0, len(s)-1
+        
+        while left < right:
+            
+            while left < right and not self.isAlphaNumeric(s[left]):
+                left += 1
+            
+            while right > left and not self.isAlphaNumeric(s[right]):
+                right -= 1
+            
+            if s[left].lower() != s[right].lower():
+                return False
+            
+            left += 1
+            right -= 1
+        return True
+        
+        
