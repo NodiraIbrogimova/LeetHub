05ab187cc8c10e075c47d21c340a51ba48f93828
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-01-10 02:53:22 +0500

    Time: 192 ms (27.90%), Space: 16 MB (10.40%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,17 +1,23 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        # if there's only one odd count char, the s can be palindrome
-        # else, not a palindrome
-        
-        
-#         # check if 
-#         head, tail = 0, len(s)-1
-#         while head < tail:
-#             if s[head] != s[tail]:
-                
-#                 pass
-#             head += 1
-#             tail -= 1
+        # Approach 2
+        # Warm-up
+        # reverse [head+1:~head]
+        # and
+        # reverse [head:~head-1]
+        # check if any match and return result
+        s = list(s)
+        head, middle = 0, len(s)//2
+        while head < middle:
+            if s[head] != s[~head]:
+                right = s[head+1:len(s)-head]
+                left = s[head:~head]
+                return (len(right) > 0 and right == right[::-1]) or (len(left) > 0 and left == left[::-1])
+            head += 1
+        return True
+            
+        # Approach 1
+        # from discussion
         p1=0
         p2=len(s)-1
         while p1<=p2:
