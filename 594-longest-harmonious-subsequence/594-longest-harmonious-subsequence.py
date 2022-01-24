08f7d17ec84aceeae9b57848f72d380fe1a2d15c
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-01-24 20:12:06 +0500

    Time: 657 ms (8.11%), Space: 16 MB (55.33%) - LeetHub

---
+++ 594-longest-harmonious-subsequence/594-longest-harmonious-subsequence.py
@@ -0,0 +1,58 @@
+class Solution:
+    def findLHS(self, nums: List[int]) -> int:
+        hashmap = Counter(nums)
+        # print('hasmap: ', hashmap)
+        max_length = 0
+        for curr in hashmap:
+            if hashmap.get(curr+1):
+                max_length = max(max_length, hashmap[curr+1]+hashmap[curr])
+            elif hashmap.get(curr-1):
+                max_length = max(max_length, hashmap[curr-1]+hashmap[curr]) 
+        return max_length
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        hashmap = dict()
+        longest_subs = 0
+        i = 0
+        while i < len(nums):
+            hashmap[nums[i]] = hashmap.get(nums[i], 0) + 1
+            if hashmap.get(nums[i]+1):
+                longest_subs = max(hashmap[nums[i]] + hashmap[nums[i] + 1], longest_subs)
+            if hashmap.get(nums[i]-1):
+                longest_subs = max(hashmap[nums[i]] + hashmap[nums[i] - 1], longest_subs)
+            i += 1
+        return longest_subs
