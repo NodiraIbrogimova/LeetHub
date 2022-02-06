03331da27537d777369fb52214aaf3b93f80816b
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-06 14:42:33 +0500

    Time: 110 ms (52.49%), Space: 14.8 MB (81.10%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.py
+++ 566-reshape-the-matrix/566-reshape-the-matrix.py
@@ -1,8 +1,56 @@
 class Solution:
     def matrixReshape(self, nums: List[List[int]], r: int, c: int) -> List[List[int]]:
-        flat = sum(nums, [])
-        if len(flat) != r * c:
+        
+        # warm-up
+        # check is rxc is possible
+        if (r*c != len(nums)*len(nums[0])) or (r == len(nums) and c == len(nums[0])):
             return nums
-        tuples = zip(*([iter(flat)] * c))
-        return map(list, tuples)
+        reshaped = []
+        for row in range(r):
+            reshaped.append([0]*c)
+        nums_row, nums_col = 0, 0
+        row, col = 0, 0
+        while row < r:
+            while col < c:
+                reshaped[row][col] = nums[nums_row][nums_col]
+                col += 1
+                nums_col += 1
+                if nums_col == len(nums[0]):
+                    nums_col = 0
+                    nums_row += 1
+            col = 0
+            row += 1
+        return reshaped
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # flat = sum(nums, [])
+        # if len(flat) != r * c:
+        #     return nums
+        # tuples = zip(*([iter(flat)] * c))
+        # return map(list, tuples)
         
