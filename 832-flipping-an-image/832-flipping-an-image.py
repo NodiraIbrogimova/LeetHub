6dc2b7f22b77e09eafee231235129daa35bc221b
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-07 19:44:38 +0500

    Time: 106 ms (5.54%), Space: 13.9 MB (94.33%) - LeetHub

--- 832-flipping-an-image/832-flipping-an-image.py
+++ 832-flipping-an-image/832-flipping-an-image.py
@@ -1,13 +1,57 @@
 class Solution:
     def flipAndInvertImage(self, image):
-        row, col = 0, 0
-        while row < len(image):
-            col = 0
-            while col < (len(image[row]) + 1)//2:
-                image[row][col], image[row][~col] = image[row][~col] ^ 1, image[row][col] ^ 1
-                col += 1
-            row += 1
+        # Warm-up for ompleting C0
+        # Approach 2
+        # 1. Flip the image horizontally
+        # 2. In step 1, complete inverse as well
+        middle = len(image[0]) % 2
+        for row in range(len(image)):
+            for col in range(len(image[0])//2 + middle):
+                image[row][col], image[row][~col] = 1-image[row][~col], 1-image[row][col]
         return image
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # row, col = 0, 0
+        # while row < len(image):
+        #     col = 0
+        #     while col < (len(image[row]) + 1)//2:
+        #         image[row][col], image[row][~col] = image[row][~col] ^ 1, image[row][col] ^ 1
+        #         col += 1
+        #     row += 1
+        # return image
     
         '''
         Time complexity: O(N) - N is total number of elements in image
