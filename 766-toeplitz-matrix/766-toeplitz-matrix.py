ff4ebdc3efa568d40e80ddb313a0ba739810049f
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-07 14:03:34 +0500

    Time: 161 ms (13.16%), Space: 13.9 MB (90.41%) - LeetHub

--- 766-toeplitz-matrix/766-toeplitz-matrix.py
+++ 766-toeplitz-matrix/766-toeplitz-matrix.py
@@ -1,6 +1,53 @@
 class Solution:
     def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
-        return all(r == 0 or c == 0 or matrix[r-1][c-1] == val
-                   for r, row in enumerate(matrix)
-                   for c, val in enumerate(row))
+        row, col = 0, 0
+        while row < len(matrix):
+            curr_row = row + 1
+            while col < len(matrix[0]):
+                curr_col = col+1
+                while curr_row < len(matrix) and curr_col < len(matrix[row]):
+                    if matrix[curr_row-1][curr_col-1] != matrix[curr_row][curr_col]:
+                        return False
+                    curr_row += 1
+                    curr_col += 1
+                curr_row = row + 1
+                col += 1
+            col = 0
+            row += 1
+        return True
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # return all(r == 0 or c == 0 or matrix[r-1][c-1] == val
+        #            for r, row in enumerate(matrix)
+        #            for c, val in enumerate(row))
         
