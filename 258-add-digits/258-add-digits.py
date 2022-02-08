1abe677d2fdf9ca95d1506ba81e005b6524ce14f
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-08 23:58:21 +0500

    Time: 39 ms (53.25%), Space: 14 MB (81.53%) - LeetHub

---
+++ 258-add-digits/258-add-digits.py
@@ -0,0 +1,21 @@
+class Solution:
+    def addDigits(self, num: int) -> int:
+        if num == 0:
+            return 0
+        if num % 9 == 0:
+            return 9
+        return num % 9
+        asum = 0
+        result = 0
+        while num > 0:
+            asum += num % 10
+            result += asum % 10
+            asum //= 10
+            num //= 10
+            
+        # result = 0
+        total = 0
+        while result > 0:
+            total += result % 10
+            result //= 10
+        return total
