9fcc5a2936818e2406dff3e65a3973d6f0f21c54
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-16 12:17:46 +0500

    Time: 1213 ms (12.79%), Space: 14.9 MB (34.56%) - LeetHub

--- 661-image-smoother/661-image-smoother.py
+++ 661-image-smoother/661-image-smoother.py
@@ -1,5 +1,58 @@
 class Solution:
-    def imageSmoother(self, M: List[List[int]]) -> List[List[int]]:
+    def is_inrange(self, position, border):
+        return not (position < 0 or position >= border)
+    
+    def imageSmoother(self, img: List[List[int]]) -> List[List[int]]:
+        # Approach 2
+        # Using hashtable to calculate the existance of neighbor
+        # at row (r,c): 
+        # (r-1, c-1), (r-1, c), (r-1, c+1), (r, c-1), (r,c), (r, c+1), (r+1, c-1), (r+1, c), (r+1, c+1)
+        filtered = [[0]*len(img[0]) for _ in range(len(img))]
+        rows = [-1,-1,-1,0,0,0,1,1,1]
+        cols = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+        row, col = 0, 0
+        while row < len(img):
+            while col < len(img[0]):
+                total = 0
+                neighbors_count = 0
+                for i in range(9):
+                    curr_row = row + rows[i]
+                    curr_col = col + cols[i]
+                    
+                    if self.is_inrange(curr_row, len(img)) and self.is_inrange(curr_col, len(img[0])):
+                        total += img[curr_row][curr_col]
+                        neighbors_count += 1
+                    
+                filtered[row][col] = total//neighbors_count
+                col += 1
+            col = 0
+            row += 1
+        return filtered
+    
+        '''
+        rows = [-1,-1,-1,0,0,0,1,1,1]
+        cols = [-1, 0, 1, -1, 0, 1, -1, 0, 1]
+        
+        for i in range(-1,2): # -1 0 1
+            for j in range(-1,2): # -1 0 1
+                nr = r + i
+                nc = c + j
+                if (nr < 0 || nr >= m) continue
+                if (nc < 0 || nc >= n) continue
+                
+        OR
+        
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                nr = row + rows[i]
+                nr = col + cols[j]
+                
+                if (nr < 0 || nr >= m) continue
+                if (nc < 0 || nc >= n) continue
+        
+        '''
+        # Approach 1
+        # Checking nine sides manually each time with if cases
         smoothed_M = deepcopy(M)
         row = 0
         while row < len(smoothed_M):
@@ -11,20 +64,22 @@ class Solution:
                 if col+1 < len(smoothed_M[row]):
                     asum += M[row][col+1]
                     neighbors += 1
+                if col-1 >= 0:
+                    asum += M[row][col-1]
+                    neighbors += 1
+                    
                 if row+1 < len(smoothed_M):
                     if col+1 < len(smoothed_M[row]):
                         # add value at the bottom diagonal
                         asum += M[row+1][col+1]
                         neighbors += 1
-                    if col - 1 >= 0:
+                    if col-1 >= 0:
                         asum += M[row+1][col-1]
                         neighbors += 1
                     # add value at the bottom
                     asum += M[row+1][col]
                     neighbors += 1
-                if col-1 >= 0:
-                    asum += M[row][col-1]
-                    neighbors += 1
+                
                 if row-1 >= 0:
                     if col-1 >= 0:
                         # add value at the top diagonal
