47c76ee74a3440c1b42235ed424491fd481569fa
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-19 05:34:41 +0500

    Time: 75 ms (63.91%), Space: 13.8 MB (98.99%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -1,5 +1,21 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        # warm-up
+        # TC: O(log10)
+        # SC: O(1)
+        if x < 0:
+            return False
+        opposite = 0
+        value = x
+        while x > 0:
+            remainder = (x - x//10*10)
+            opposite = opposite*10 + remainder
+            x //= 10
+        return opposite == value
+        
+        # Approach 2
+        # Wrap to string and check with its opposite
+        # very slow
         if x < 0:
             return False
         return str(x) == str(x)[::-1]
