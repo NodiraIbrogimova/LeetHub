97eaa24a35204989c762b312d6888e48e9dc4ee5
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-19 08:43:49 +0500

    Time: 497 ms (90.26%), Space: 14.3 MB (89.32%) - LeetHub

--- 463-island-perimeter/463-island-perimeter.py
+++ 463-island-perimeter/463-island-perimeter.py
@@ -1,78 +1,15 @@
 class Solution:
-    perimeter = 0
-    def sumPerimeter(self, row, col, grid):
-        if col - 1 < 0:
-            self.perimeter += 1
-        elif grid[row][col-1] == 0:
-            self.perimeter += 1
-            
-        if col + 1 >= len(grid[row]):
-            self.perimeter += 1
-        elif grid[row][col+1] == 0:
-            self.perimeter += 1
-        
-        if row - 1 < 0:
-            self.perimeter += 1
-        elif grid[row-1][col] == 0:
-            self.perimeter += 1
-            
-        if row + 1 >= len(grid):
-            self.perimeter += 1
-        elif grid[row+1][col] == 0:
-            self.perimeter += 1
-    
     def islandPerimeter(self, grid: List[List[int]]) -> int:
-        row, col = 0,0
-        while row < len(grid):
-            
-            while col < len(grid[row]):
-                land = grid[row][col]
-                if not land:
-                    col += 1
-                    continue
-                # check four sides
-                self.sumPerimeter(row, col, grid)
-                col += 1
-            col = 0
-            row += 1
-        
-        return self.perimeter
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        grid_ext = ['0' + ''.join(str(x) for x in row) + '0' for row in grid]
-        grid_trans = list(map(list, zip(*grid)))
-        grid_ext += [ '0' + ''.join(str(x) for x in row) + '0' for row in grid_trans ]                
-        return sum(row.count('01') + row.count('10') for row in grid_ext)
-        
-        
+        perimeter = 0
+        rows, cols = len(grid), len(grid[0])
+        for row in range(rows):
+            for col in range(cols):
+                if grid[row][col] == 1:
+                    # left
+                    if col > 0 and grid[row][col-1] == 1:
+                        perimeter -= 2
+                    # bottom
+                    if row > 0 and grid[row-1][col] == 1:
+                        perimeter -= 2				
+                    perimeter += 4
+        return perimeter
