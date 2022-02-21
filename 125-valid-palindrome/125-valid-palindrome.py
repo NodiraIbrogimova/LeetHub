499a4d348bba913141f9ece97d330602f3eabce5
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-21 19:52:26 +0500

    Time: 69 ms (47.03%), Space: 14.4 MB (80.74%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.py
+++ 125-valid-palindrome/125-valid-palindrome.py
@@ -3,6 +3,21 @@ class Solution:
         return (ord('A') <= ord(value) <= ord('Z')) or (ord('a') <= ord(value) <= ord('z')) or (ord('0') <= ord(value) <= ord('9'))
     
     def isPalindrome(self, s: str) -> bool:
+        left, right = 0, len(s)-1
+        while left < right:	
+            # iterate left and right until char in  [48:57], [65,90], [97:122] ranges met
+            while left < right and not s[left].isalnum():
+                left += 1
+            while right > left and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
+        
+        
+        
         '''
         What are the characteristics of a palindrome?
         1. Check if a value is alphanumeric for both left and right pointers.
