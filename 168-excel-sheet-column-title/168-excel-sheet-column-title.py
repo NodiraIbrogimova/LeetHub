7f064361321e3cd5785d7afe55787927e729922a
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-02-28 21:41:38 +0500

    Time: 46 ms (41.89%), Space: 14 MB (44.71%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -1,24 +1,96 @@
 class Solution:
-    def convertToTitle(self, num: int) -> str:
+    def convertToTitle(self, columnNumber: int) -> str:
         '''
-        173659
+        A = ord('A')
         
-        65-90
-        
+        A...Z = 1...26
+        AA...AZ = 27...52
+        BA...BZ = 53...78
+        CA....CZ = 79...104
+        53 = 2,1 = [2//53 <= 2] chr(2%26**2 + A - 1)  +  [53//26 <= 53] chr(53%26**1+A-1)
+        54 = 2,2 = [2//54 <= 2] chr(2%26**2 + A - 1)  +  [54//26 <= 54] chr(54%26**1+A-1)
+        55 = 2,3
+        ...
+        78 =  1,26
+        104 = 3,26
+        703 = 1,1,1 = (703%26**1+A)
         '''
-        # A = ord('A')
-        # result = []
-        # while columnNumber > 0:
-        #     value = columnNumber % 10
-        #     # print('val: ', value, A+value-1)
-        #     result.append(chr(A+value-1))
-        #     columnNumber //= 10
-        # return "".join(result[::-1])
-    
-        capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
+        A = ord('A')
+        result = list()
+        while columnNumber > 0:
+            columnNumber -= 1
+            result.append(chr(columnNumber%26 + A))
+            columnNumber //= 26
+        return "".join(result[::-1])
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        result = list()
+        A = ord('A')
+        remainder = 0
+        while remainder > 0:
+            remainder = columnNumber%26
+            result.append(chr(A + remainder - 1))
+            columnNumber -= remainder
+        if columnNumber >= 26:
+            result.append(chr(A + columnNumber%26 - 1))
+        return "".join(result)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        print('res: ', 701//26, 701%26)
+        A = ord('A')
         result = []
-        while num > 0:
-            result.append(capitals[(num-1)%26])
-            num = (num-1) // 26
-        result.reverse()
-        return ''.join(result)
+        
+        while columnNumber > 0:
+            value = columnNumber // 26 + A
+            print('value: ', value)
+            result.append(chr(value))
+            print('columnNumber: ', columnNumber)
+            columnNumber %= 26
+        return "".join(result[::-1])
