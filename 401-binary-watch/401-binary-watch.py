78a15b829ea013cc78b7e0c6338b56c2e9b25552
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-03-13 23:57:31 +0500

    Time: 46 ms (57.69%), Space: 13.9 MB (46.75%) - LeetHub

--- 401-binary-watch/401-binary-watch.py
+++ 401-binary-watch/401-binary-watch.py
@@ -1,6 +1,55 @@
 class Solution:
-    def readBinaryWatch(self, num: int) -> List[str]:
+    def readBinaryWatch(self, turnedOn: int) -> List[str]:
         return ['%d:%02d' % (h, m)
             for h in range(12) for m in range(60)
-            if (bin(h) + bin(m)).count('1') == num]
+            if (bin(h) + bin(m)).count('1') == turnedOn]
+        if turnedOn > 8:
+            return []
+        result = []
+        
+        def dfs(self, turnedOn):
+            if turnedOn <= 0:
+                return
+            
+        
+        self.dfs(turnedOn)
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        # return ['%d:%02d' % (h, m)
+        #     for h in range(12) for m in range(60)
+        #     if (bin(h) + bin(m)).count('1') == num]
         
