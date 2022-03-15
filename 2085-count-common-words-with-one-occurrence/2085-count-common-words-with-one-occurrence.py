41f44f1f3d3d46756ef42063771e8c69610c4cfe
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-03-15 12:12:08 +0500

    Time: 111 ms (55.63%), Space: 14.5 MB (30.14%) - LeetHub

--- 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
+++ 2085-count-common-words-with-one-occurrence/2085-count-common-words-with-one-occurrence.py
@@ -1,5 +1,20 @@
 class Solution:
     def countWords(self, words1: List[str], words2: List[str]) -> int:
+        # Approach 2
+        # Using sungle hashmap
+        hashmap = Counter(words1)
+        for word in words2:
+            if word in hashmap and hashmap[word] < 2:
+                hashmap[word] -= 1
+        count = 0
+        for word in words2:
+            if hashmap.get(word, -1) == 0:
+                count += 1
+        return count
+        
+        
+        # Approach 1
+        # Using two hashmaps
         hashmap1 = Counter(words1)
         hashmap2 = Counter(words2)
         count = 0
