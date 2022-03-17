7cfc2e75a6b5211e25deb7c4d5930c2dfea62d0e
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-03-17 20:24:40 +0500

    Time: 53 ms (67.47%), Space: 14 MB (41.40%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.py
@@ -1,17 +1,34 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
+        # Approach 2: From Discussions
+        
+        # The elements on left of k come first, so the time spent on them are always calculated
+        # as they are first in the queue
+        
+        # The elements coming akter kth elements might not be waited for 
+        # them to complete getting orders as by that time kth buyer might 
+        # have finished buying all tickets
+        # That's why count them as one second less than the kth number or itself if < tickets[k]
+        
+        time_spent = 0
+        for i in range(k+1):
+            time_spent += min(tickets[k], tickets[i])
+        for i in range(k+1, len(tickets)):
+            time_spent += min(tickets[k]-1, tickets[i])
+        return time_spent
+    
         # Approach 1
         # Iterate through the array until tickets[k] becomes 0
         # In each iteration, inc asum with 1sec for each element
         # In case tickets[i] becomes 0 at certain point, return sum directly
-        spent = 0
+        time_spent = 0
         while tickets[k] > 0:
             i = 0
             while i < len(tickets):
                 if tickets[k] == 0:
-                    return spent
+                    return time_spent
                 if tickets[i] > 0:
                     tickets[i] -= 1
-                    spent += 1
+                    time_spent += 1
                 i += 1
-        return spent
+        return time_spent
