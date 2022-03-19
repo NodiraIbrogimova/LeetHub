50b739ca829c24768f7c21bf124bb5d6c5633f63
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-03-19 22:56:53 +0500

    Time: 56 ms (30.25%), Space: 13.9 MB (60.02%) - LeetHub

--- 258-add-digits/258-add-digits.py
+++ 258-add-digits/258-add-digits.py
@@ -1,6 +1,23 @@
 class Solution:
     # asum = 0
     def addDigits(self, num: int) -> int:
+        # Approach 3
+        # Recursive approach: divide num and sum until num becomes < 10
+        # base case: if the given val becomes less than 10, return value
+        '''
+        base case: when val < 1
+        recursive case: 
+        Keep adding digits until asum becomes < 10
+        '''
+        asum = 0
+        while num > 0:
+            asum += num % 10
+            num //= 10
+        if asum < 10:
+            return asum
+        return self.addDigits(asum)
+        
+        
         # Approach 2
         # Iterative, brute-force
         # TC: O(n)
