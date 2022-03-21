bf88da0ba292fbf5350ce6e16dc9a1e8cb4e5357
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-03-21 17:50:53 +0500

    Time: 2484 ms (88.02%), Space: 34.2 MB (14.03%) - LeetHub

--- 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
+++ 2196-create-binary-tree-from-descriptions/2196-create-binary-tree-from-descriptions.py
@@ -1,13 +1,16 @@
 # Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         # Step 1
         children  = set()
+        for parent, child, isLeft in descriptions:
+            if child not in children:children.add(child)
+    
         hashmap = dict()
         for key in descriptions:
             hashmap[key[0]] = [None, None]
@@ -21,9 +24,14 @@ class Solution:
                 hashmap[parent] = [hashmap.get(parent)[0], child]
                 
         # Step 2 - Finding root
-        root = (hashmap.keys() - children).pop()
-        print('root is: ', root)
-        
+        root = None
+        for parent, child, isLeft in descriptions:
+            if parent not in children:
+                root = parent
+                break
+            elif child not in children:
+                root = child
+                break
         # Step 3
         def dfs(node):
             if not node:
