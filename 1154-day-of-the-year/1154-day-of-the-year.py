b40f04cd86cb7c0300be90c54d06024a215c7fc1
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-03-22 23:25:57 +0500

    Time: 191 ms (5.98%), Space: 13.9 MB (82.05%) - LeetHub

--- 1154-day-of-the-year/1154-day-of-the-year.py
+++ 1154-day-of-the-year/1154-day-of-the-year.py
@@ -9,10 +9,9 @@ class Solution:
         3. Add the days until this day from current month
         '''
         months = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30]
-        month, day = int(date[5:7]), int(date[8:])
-        year = int(date[:4]) - 1900
-        if year > 0 and year % 4 == 0: months[2] += 1
-            
-        start = 0
+        year, month, day = map(int, date.split('-'))
+        first_year = 1900
+        year_difference = year - first_year
+        if year_difference > 0 and year_difference % 4 == 0: months[2] += 1
         for i in range(1, len(months)): months[i] += months[i-1]
         return months[month-1] + day
