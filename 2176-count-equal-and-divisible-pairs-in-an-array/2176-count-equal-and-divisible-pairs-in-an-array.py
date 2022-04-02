d5ec4d7b8c899e02070e60e435ba0124ab98016b
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-04-03 00:26:06 +0500

    Time: 85 ms (90.46%), Space: 13.9 MB (73.70%) - LeetHub

--- 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
+++ 2176-count-equal-and-divisible-pairs-in-an-array/2176-count-equal-and-divisible-pairs-in-an-array.py
@@ -1,15 +1,41 @@
 class Solution:
     def countPairs(self, nums: List[int], k: int) -> int:
         '''
-        [3,1,2,2,2,1,3]
-        
-        
+        (i * j) is divisible by k, if either i or j is divisible by k
+        - As looking for pairs, check for numbers with freq > 1
+        - count array holding freq of each num in nums
+        - if count is > 1, doesn't matter where this values are, 
+        every time there will be i < j possibility. But, we should find if i or j is divisible by k
         '''
-        # Approach 1
-        # Brute-force
-        count = 0
-        for i in range(len(nums)-1):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j] and i*j % k == 0:
-                    count += 1
+        MAX_LENGTH = 100
+        freq = [[] for i in range(MAX_LENGTH+1)]
+        for index, num in enumerate(nums):
+            freq[num].append(index)
+        count, excluded = 0, 0
+        for indexes in freq:
+            excluded = 0
+            for i in range(len(indexes)):
+                for j in range(i+1, len(indexes)):
+                    if indexes[i]*indexes[j] % k == 0:
+                        count += 1
         return count
+    
+        # Approach 1
+        # Optimized Brute-force 
+#         hashmap = dict()
+#         count = 0
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] == nums[j] and (i % k == 0 or j % k == 0):
+#                     count += 1
+#         return count
+    
+    
+#         # Approach 1
+#         # Brute-force
+#         count = 0
+#         for i in range(len(nums)-1):
+#             for j in range(i+1, len(nums)):
+#                 if nums[i] == nums[j] and (i % k == 0 or j % k == 0):
+#                     count += 1
+#         return count
