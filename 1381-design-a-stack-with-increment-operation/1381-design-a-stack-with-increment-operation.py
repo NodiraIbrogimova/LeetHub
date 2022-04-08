ebda35692355b35e62dc0ea8546c1e7e8cf89775
Nodira Ibrogimova <nodira.ibrogimova@mail.ru>
2022-04-09 00:17:33 +0500

    Time: 201 ms (24.22%), Space: 15.1 MB (7.79%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.py
@@ -1,60 +1,55 @@
-# # Approach 2
-# # Using doublylinked list
-# class Node:
+# Approach 2
+# Using doublylinked list
+class Node:
 
-#     def __init__(self, val=0, anext=None, prev=None):
-#         self.val = val
-#         self.anext = anext
-#         self.prev = prev
-       
-    
-# class CustomStack:
-    
-#     def __init__(self, maxSize):
-#         self.capacity = maxSize
-#         self.curr_size = 0
-#         self.head = None
-#         self.tail = None
-        
-#     def push(self, value):
-#         if self.curr_size == self.capacity: return -1
-#         new_node = Node(val=value)
-#         prev_node = self.head
-#         if self.curr_size == 0: # or self.curr_size == 0
-#             self.tail = new_node
-#         else:
-#             new_node.anext = prev_node
-#             prev_node.prev = new_node
-#         self.head = new_node
-#         self.curr_size += 1
-#         print('\npush: ', self.curr_size, self.head)
-#         curr = self.head
-#         while curr:
-#             print('result: ', curr.val)
-#             curr = curr.anext
-    
-#     def pop(self):
-#         if self.curr_size == 0: return -1
-#         prev_node = self.head.anext
-#         if prev_node: 
-#             prev_node.prev = None
-#             self.head.anext = None
-#         self.head = prev_node
-#         self.curr_size -= 1
-    
-#     def increment(self, k, value):
-#         # inc k number of elements in the bottom by val
-#         curr = self.tail
-#         while curr and k:
-#             print('val: ', curr.val)
-#             curr.val += value
-#             k -= 1
-#             curr = curr.anext
+    def __init__(self, val=0, anext=None, prev=None):
+        self.val = val
+        self.anext = anext
+        self.prev = prev
 
+
+class CustomStack:
+
+    def __init__(self, maxSize):
+        self.capacity = maxSize
+        self.curr_size = 0
+        self.head = None
+        self.tail = None
+
+    def push(self, value):
+        if self.curr_size == self.capacity: return -1
+        new_node = Node(val=value)
+        prev_node = self.head
+        if self.curr_size == 0:  # or self.curr_size == 0
+            self.tail = new_node
+        else:
+            new_node.anext = prev_node
+            prev_node.prev = new_node
+        self.head = new_node
+        self.curr_size += 1
+
+    def pop(self):
+        if self.curr_size == 0: return -1
+        value = self.head.val
+        prev_node = self.head.anext
+        if prev_node:
+            prev_node.prev = None
+            self.head.anext = None
+        self.head = prev_node
+        self.curr_size -= 1
+        return value
+
+    def increment(self, k, value):
+        # inc k number of elements in the bottom by val
+        curr = self.tail
+        while curr and k:
+            curr.val += value
+            k -= 1
+            curr = curr.prev
 
 # Approach 1
 # Using built-in data structure - deque
-
+'''
 class CustomStack:
     
     def __init__(self, maxSize: int):
@@ -79,7 +74,7 @@ class CustomStack:
         for i in range(len(self.stack)-increment_range, len(self.stack)):
             self.stack[i] += val
             
-
+'''
 # Your CustomStack object will be instantiated and called as such:
 # obj = CustomStack(maxSize)
 # obj.push(x)
